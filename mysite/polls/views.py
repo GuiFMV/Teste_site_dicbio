5143f0eb34e232cf959e654d02ee57b654037c57
Guilherme <guila.fmv@gmail.com>
2025-02-20 23:10:36 -0400

    adicionei scroll das palavras

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+import os
 
 
 def home_view(request):
@@ -11,9 +13,12 @@ def home_view(request):
     ]
     return render(request, 'polls/home.html', {'images': images})
 
-def consulta_view(request):
 
-    return render(request, 'polls/consulta.html')
+def consulta_view(request):
+    json_path = os.path.join("polls", "data", "polls", "json_data", "DadosDoDicionario.json")
+    with open(json_path, encoding="utf-8") as f:
+        data = json.load(f)
+    return render(request, "polls/consulta.html", {"headwords": data})
 
 
 def documentacao_view(request):
