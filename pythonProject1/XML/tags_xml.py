b9397d85d14278d7eb06455f1a349d2070247669
Guilherme <guila.fmv@gmail.com>
2025-02-22 19:32:52 -0400

    xml-tags

--- pythonProject1/XML/tags_xml.py
+++ pythonProject1/XML/tags_xml.py
@@ -1,28 +1,11 @@
 import xml.etree.ElementTree as ET
 
 # Carregar o arquivo XML
-# No meu caso, para encontrar o caminho do arquivo preciso passar o caminho completo da minha pasta e colocar duas
-# barras para entender que estou usando uma
-tree = ET.parse("C:\\Users\\Guila\\OneDrive\\Documentos\\dicbio\\data\\compendio1brotero.xml")  # Substitua pelo caminho correto
+tree = ET.parse("C:\\Users\\Guila\\OneDrive\\Documentos\\dicbio-TEI-XML\\data\\compendio1brotero.xml")
 root = tree.getroot()
 
-# for element in root.iter(): # Para teste
-#     print(element.tag, element.attrib)
+# Converter o XML para string formatada
+xml_str = ET.tostring(root, encoding="unicode")
 
-# Encontrar o elemento <body>
-body_element = root.find("front") # Escolha o elemento desejado front, body ou back
-
-# Função para extrair todo texto dentro de um elemento
-
-
-def extract_text(element):
-    text = element.text or ""
-    for sub_element in element:
-        text += extract_text(sub_element) + (sub_element.tail or "")
-    return text
-
-# Obter e imprimir o texto dentro de <body>
-
-
-body_text = extract_text(body_element)
-print(body_text)
+# Imprimir o conteúdo XML
+print(xml_str)
