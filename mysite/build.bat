1d19d62caec627d192e71831babe915add4dc262
Guilherme <guila.fmv@gmail.com>
2025-02-24 00:52:09 -0400

    hospedagem e arrumando carrossel imagens

--- mysite/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python -m ensurepip --default-pip
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lwfvvvp5mx4lf@ezdd0*9b=*hcoarzkyt+&ny3d1ppu#ad92_v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
